b1ee65a1c08dd446810440c80f86dc8eec2f64c1
agitatoh <laraals@proton.me>
2026-07-13 14:12:02 +1000

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const btn = document.getElementById("startBtn");
 const player = document.getElementById("player");
 
 const messages = [
-    "💌 I GOT A TEXT! Tonight's giving main character energy.",
-    "🌴 Welcome to the villa. Islanders, please take your seats.",
-    "💖 Warning: excessive screaming at the TV may occur.",
-    "✨ You're officially coupled up with the Season Finale."
+    "📱 I GOT A TEXT! Your finale has officially arrived.",
+    "🍿 Everyone settle in... it's finally happening.",
+    "💖 No buffering. No ads. Just vibes.",
+    "🥂 May your favourite couple win.",
+    "🌴 Enjoy!! Love you girls xx"
 ];
 
 btn.addEventListener("click", () => {
@@ -14,7 +15,7 @@ btn.addEventListener("click", () => {
     player.classList.remove("hidden");
 
     document.querySelector(".message").innerHTML =
-        messages[Math.floor(Math.random()*messages.length)];
+        messages[Math.floor(Math.random() * messages.length)];
 
     document.querySelector("video").play();
 
