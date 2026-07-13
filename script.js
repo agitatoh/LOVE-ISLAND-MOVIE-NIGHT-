a36103bc9fa3d6a61209f3611dccfe92e9541e24
agitatoh <laraals@proton.me>
2026-07-13 14:57:16 +1000

    Update script.js

--- script.js
+++ script.js
@@ -6,30 +6,29 @@ const card = document.getElementById("card");
 
 btn.addEventListener("click", async () => {
 
-
+    // hide button
     btn.style.display = "none";
 
-
+    // show video
     player.classList.remove("hidden");
 
-
+    // turn into viewing mode
     card.classList.add("watching");
 
 
+    // attempt fullscreen
     try {
 
         await card.requestFullscreen();
 
-    }
-
-    catch(error){
+    } catch(error){
 
         console.log("Fullscreen unavailable");
 
     }
 
 
+    // start video
     video.play();
-
 
 });
